d426ec15e24ce9385b65934434f57df1914ccee1
mayd21 <mayd21@qq.com>
2019-05-06 19:17:38 +0800

    Add notes

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -1,3 +1,6 @@
+/**
+ * Vue实例构造函数
+ */
 import { initMixin } from './init'
 import { stateMixin } from './state'
 import { renderMixin } from './render'
@@ -11,13 +14,18 @@ function Vue (options) {
   ) {
     warn('Vue is a constructor and should be called with the `new` keyword')
   }
+  // _init来自initMixin
   this._init(options)
 }
-
+// 初始化混入
 initMixin(Vue)
+// props、methods、data、compute、watch的状态初始化
 stateMixin(Vue)
+// $on、$once、$off、$emit事件函数初始化
 eventsMixin(Vue)
+// 初始化生命周期
 lifecycleMixin(Vue)
+// $nextTick、_render函数初始化
 renderMixin(Vue)
 
 export default Vue
